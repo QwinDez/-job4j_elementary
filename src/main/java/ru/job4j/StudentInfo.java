bf0516904e10259a954fc4eac97b3052ed41468f
Den <dezer1993@yandex.ru>
2021-12-04 16:15:30 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Denis Soldatkin");
+        System.out.println("06.07.1993");
+
+    }
+}
