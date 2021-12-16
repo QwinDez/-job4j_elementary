a2d8d8412985561bc406631acc52f78423c5a6ca
Den <dezer1993@yandex.ru>
2021-12-16 23:50:53 +0300

    6.0.1. Объявление массива. [#3613]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,10 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+
+    public static void main(String[] args) {
+        short[] element = new short[10];
+        String[] strings = new String[100500];
+        float[] number = new float[40];
+    }
+}
