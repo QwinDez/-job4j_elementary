4d22bc269c632d272167d2ae9a884817af5de632
Den <dezer1993@yandex.ru>
2021-12-27 19:40:52 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -12,4 +12,14 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int index = 0; index < data.length; index++) {
+            if (index >= start && index <= finish && data[index] == el) {
+                rst = index;
+            }
+        }
+        return rst;
+    }
 }
